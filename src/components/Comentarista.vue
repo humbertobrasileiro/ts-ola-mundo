6b2a3aa2d27dad921f81b922d8c55383e8d5eb23
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-07-20 17:46:30 -0300

    primeiro commit

---
+++ src/components/Comentarista.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="comentar">
+      Comentar
+    </button>
+    <p>
+      {{ comentario }}
+    </p>
+    <p>{{ detalhes }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  import { Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class Comentarista extends Vue {
+    private comentario = '';
+    @Prop() private nome: string;
+
+    private comentar() : void {
+      this.comentario = 'Um mega comentário bem bacana!';
+    }
+
+    get detalhes () {
+      return `Nome do comentarista: ${this.nome}`;
+    }
+
+  }
+
+</script>
